7653196491aad24af2d0bffc14a0854d60037ace
Rajveer Singh <125016453+Rajveer-Singh8124@users.noreply.github.com>
2024-10-29 13:06:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import numpy as np
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
 
-app = Flask(__name__,static_folder="forest-fire-app/build",static_url_path="/")
+app = Flask(__name__,static_folder="front end/build",static_url_path="/")
 CORS(app) 
 
 # Load the model and scaler
