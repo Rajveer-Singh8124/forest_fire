2baa45e918b2d70fa06aa713668987c55a4b205c
Rajveer Singh <125016453+Rajveer-Singh8124@users.noreply.github.com>
2024-10-29 12:58:35 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, request, jsonify,send_from_directory
+import os
+from flask_cors import CORS  
+import pickle
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+
+app = Flask(__name__,static_folder="forest-fire-app/build",static_url_path="/")
+CORS(app) 
+
+# Load the model and scaler
+model = pickle.load(open("models/linear_model.pkl", "rb"))
+scaler = pickle.load(open("models/scaler.pkl", "rb"))
+
+@app.route("/api/predict", methods=["POST"])
+def predict():
+    data = request.get_json()
+    
+    input_data = pd.DataFrame([data])
+    
+    scaled_data = scaler.transform(input_data)
+    
+    prediction = model.predict(scaled_data)
+    
+    response = {
+        "prediction": str(prediction[0]) }
+    return jsonify(response)
+
+
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def serve(path):
+    return send_from_directory(app.static_folder, path) if path else send_from_directory(app.static_folder, 'index.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
